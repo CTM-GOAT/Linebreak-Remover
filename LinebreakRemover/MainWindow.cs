ab76271fd586b762d598c0aa5a283755481b19ac
Alex Magee <alex.magee16701@gmail.com>
2020-01-17 18:51:10 -0500

    Initial project commit
    Initial commit

---
+++ LinebreakRemover/MainWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    { 
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBtnRemoveBreaksClicked(object sender, EventArgs e)
+    {
+        TextboxProgramOutput.Buffer.Text = "";
+        string SingleLineString = RemoveBreaks(TextboxUserEntry.Buffer.Text);
+        TextboxProgramOutput.Buffer.Text = SingleLineString;
+    }
+
+    protected void OnBtnClearClicked(object sender, EventArgs e)
+    {
+        TextboxProgramOutput.Buffer.Text = "";
+        TextboxUserEntry.Buffer.Text = "";
+    }
+
+    static string RemoveBreaks(string InputText)
+    {
+        return Regex.Replace(InputText, @"\t|\n|\r", " ");
+    }
+
+}
